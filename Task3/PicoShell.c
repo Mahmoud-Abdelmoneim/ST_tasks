38d120abbd7031a05a3992f205a77751cece42a0
Mahmoud Abdelmoniem <109484204+Mahmoud-Abdelmoneim@users.noreply.github.com>
2024-02-05 21:31:03 +0200

    Update PicoShell.c

--- Task3/PicoShell.c
+++ Task3/PicoShell.c
@@ -5,24 +5,51 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_INPUT_SIZE 1024
 #define MAX_ARGUMENTS 10
 
-void parseCommand(char *input, char *arguments[]) {
+void parseCommand(char *input, char ***arguments) {
     int i = 0;
     char *token = strtok(input, " \n");
 
-    while (token != NULL && i < MAX_ARGUMENTS) {
-        arguments[i++] = token;
+    while (token != NULL && i < MAX_ARGUMENTS - 1) {
+        (*arguments)[i++] = strdup(token);
         token = strtok(NULL, " \n");
     }
 
-    arguments[i] = NULL; 
+    (*arguments)[i] = NULL;
+}
+
+void freeArguments(char **arguments) {
+    for (int i = 0; arguments[i] != NULL; i++) {
+        free(arguments[i]);
+    }
+    free(arguments);
+}
+
+void executeCommand(char **arguments) {
+    pid_t returned_pid = fork();
+
+    if (returned_pid < 0) {
+        perror("fork error");
+        exit(EXIT_FAILURE);
+    } else if (returned_pid == 0) {
+        if (execvp(arguments[0], arguments) == -1) {
+            perror("execvp error");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        int wstatus;
+        if (waitpid(returned_pid, &wstatus, 0) == -1) {
+            perror("waitpid error");
+            exit(EXIT_FAILURE);
+        }
+    }
 }
 
 int main() {
-    char input[MAX_INPUT_SIZE];
-    char *arguments[MAX_ARGUMENTS];
+    char *input = NULL;
+    size_t input_size = 0;
+    char **arguments = NULL;
 
     while (1) {
         // Print shell prompt
@@ -30,7 +57,7 @@ int main() {
         fflush(stdout);
 
         // Read user input
-        if (fgets(input, sizeof(input), stdin) == NULL) {
+        if (getline(&input, &input_size, stdin) == -1) {
             perror("Error reading input");
             exit(EXIT_FAILURE);
         }
@@ -39,19 +66,19 @@ int main() {
         input[strcspn(input, "\n")] = '\0';
 
         // Parse the command
-        parseCommand(input, arguments);
+        arguments = (char **)malloc(MAX_ARGUMENTS * sizeof(char *));
+        parseCommand(input, &arguments);
 
         // Check if the user entered a command
         if (arguments[0] != NULL) {
-            
             if (strcmp(arguments[0], "echo") == 0) {
-                // Print arguments after 'echo'
+                // Handle 'echo' command
                 for (int i = 1; arguments[i] != NULL; i++) {
                     printf("%s ", arguments[i]);
                 }
                 printf("\n");
             } else if (strcmp(arguments[0], "pwd") == 0) {
-                // Print the current working directory
+                // Handle 'pwd' command
                 char cwd[1024];
                 if (getcwd(cwd, sizeof(cwd)) != NULL) {
                     printf("%s\n", cwd);
@@ -59,45 +86,30 @@ int main() {
                     perror("getcwd() error");
                 }
             } else if (strcmp(arguments[0], "cd") == 0) {
-                // Change directory
+                // Handle 'cd' command
                 if (arguments[1] != NULL) {
                     if (chdir(arguments[1]) != 0) {
-                        perror("cd error");
+                        perror("chdir error");
                     }
                 } else {
                     fprintf(stderr, "cd: missing argument\n");
                 }
             } else if (strcmp(arguments[0], "exit") == 0) {
-                // Exit the shell
+                // Handle 'exit' command
+                freeArguments(arguments);
+                free(input);
                 exit(EXIT_SUCCESS);
             } else {
-                //printf("this command not supported now!\n");
-                	
-               pid_t returned_pid = fork();
-               if (returned_pid > 0) {
-                   int wstatus;
-  
-                   wait(&wstatus);
-               } else if (returned_pid == 0) {
-      
-		
-                   char *new_program_argv[100] = {arguments[0],arguments[1]};
-                   
-                   for (int i = 1; arguments[i] != NULL; i++) {
-                      new_program_argv[i] = arguments[i];
-                      //printf("==========%s",arguments[1]);
-                   }
+                // Execute external command
+                executeCommand(arguments);
+            }
 
-                   
-                   execvp(arguments[0], new_program_argv);
-                   
-                   printf("I am not in the mode of execution. Exec failed\n");
-            }
-                                    
-               
-            }
+            // Free memory allocated for arguments
+            freeArguments(arguments);
         }
     }
+
+    free(input);  // Free memory allocated for input
 
     return 0;
 }
